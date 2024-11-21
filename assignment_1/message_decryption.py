3457b6ecc422de85f67184b075b410de418f60fd
MiaHenstridge <nongngocmai22@gmail.com>
2024-11-21 11:07:06 +0100

    modified mesage encryption and decryption script

--- assignment_1/message_decryption.py
+++ assignment_1/message_decryption.py
@@ -1,23 +1,39 @@
-#%%
+# %%
 from pathlib import Path
+
 # RSA encryption modules from pycryptodome library
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
+
 # %%
-#%%
+# %%
 # Define paths for our key files
 PROJECT_FOLDER = Path(__file__).parent.parent
-# PRIVATE_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1"  # Contains the private key
-PUBLIC_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1.pub"  # Contains the public key
+PRIVATE_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1"  # Contains the private key
+# PUBLIC_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1.pub"  # Contains the public key
 
 # %%
 # Make sure our key files exist before proceeding
-# assert Path.exists(PRIVATE_KEY_FILE)
-assert Path.exists(PUBLIC_KEY_FILE)
+assert Path.exists(PRIVATE_KEY_FILE)
+# assert Path.exists(PUBLIC_KEY_FILE)
 
 # %%
 # Load the public key to decrypt the message
-with open(PUBLIC_KEY_FILE, 'r', encoding='utf-8') as key_file:
-    public_key = RSA.import_key(key_file.read())
+with open(PRIVATE_KEY_FILE, "r", encoding="utf-8") as key_file:
+    private_key = RSA.import_key(key_file.read())
+
+# %%
+ENCRYPTED_MESSAGE_FILE = PROJECT_FOLDER / "encrypted_message_suncica.bin"
+# Read the encrypted message from file
+with open(ENCRYPTED_MESSAGE_FILE, "rb") as f:
+    encrypted_message_from_file = f.read()
+
+# %%
+# Create a cipher object using the private key for decryption
+private_key_cipher = PKCS1_OAEP.new(private_key)
+
+# Decrypt the message using the private key
+decrypted_message = private_key_cipher.decrypt(encrypted_message_from_file)
+print(f"Decrypted message: {decrypted_message.decode('utf-8')}")
 
 # %%
--- assignment_1/message_encryption.py
+++ assignment_1/message_encryption.py
@@ -7,25 +7,25 @@ from Crypto.PublicKey import RSA
 #%%
 # Define paths for our key files
 PROJECT_FOLDER = Path(__file__).parent.parent
-PRIVATE_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1"  # Contains the private key
-# PUBLIC_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1.pub"  # Contains the public key
+# PRIVATE_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1"  # Contains the private key
+PUBLIC_KEY_FILE = PROJECT_FOLDER / "my_keypair_assignemt_1.pub"  # Contains the public key
 # %%
 # Make sure our key files exist before proceeding
-assert Path.exists(PRIVATE_KEY_FILE)
-# assert Path.exists(PUBLIC_KEY_FILE)
+# assert Path.exists(PRIVATE_KEY_FILE)
+assert Path.exists(PUBLIC_KEY_FILE)
 # %%
 # Load the private key to encrypt the message
-with open(PRIVATE_KEY_FILE, 'r', encoding='utf-8') as key_file:
-    private_key = RSA.import_key(key_file.read())
+with open(PUBLIC_KEY_FILE, 'r', encoding='utf-8') as key_file:
+    public_key = RSA.import_key(key_file.read())
 # %%
 # message to be encrypted
 short_secret_message = "Hello World!".encode('utf-8')
 # %%
 # Create a cipher object using the private key for encryption
-private_key_cipher = PKCS1_OAEP.new(private_key)
+public_key_cipher = PKCS1_OAEP.new(public_key)
 # %%
 # Encrypt our message - only someone with the public key can decrypt it
-encrypted_message = private_key_cipher.encrypt(short_secret_message)
+encrypted_message = public_key_cipher.encrypt(short_secret_message)
 print(f"Encrypted message:")
 print(encrypted_message)
 # %%
@@ -34,7 +34,9 @@ ENCRYPTED_MESSAGE_FILE = PROJECT_FOLDER / "encrypted_message_assignment1.bin"
 with open(ENCRYPTED_MESSAGE_FILE, "wb") as f:
     f.write(encrypted_message)
 # %%
-ENCRYPTED_MESSAGE_FILE = PROJECT_FOLDER / "encrypted_message.bin"
+ENCRYPTED_MESSAGE_FILE = PROJECT_FOLDER / "encrypted_message_assignment1.bin"
 # Read the encrypted message from file
 with open(ENCRYPTED_MESSAGE_FILE, "rb") as f:
     encrypted_message_from_file = f.read()
+
+# %%
