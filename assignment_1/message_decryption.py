cb43ab2c10b2e7d4b6675bc282fe1ad475d39cc7
Mai Nong (Mia) <85831304+MiaHenstridge@users.noreply.github.com>
2024-11-22 20:27:39 +0000

    cleaned up message decryption script

--- assignment_1/message_decryption.py
+++ assignment_1/message_decryption.py
@@ -4,10 +4,11 @@ from pathlib import Path
 # RSA encryption modules from pycryptodome library
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
+
 # %%
 # Define paths for our key files
 PROJECT_FOLDER = Path(__file__).parent.parent
-PRIVATE_KEY_FILE = PROJECT_FOLDER / "ceu_key (1).txt"  # Contains the private key
+PRIVATE_KEY_FILE = PROJECT_FOLDER / "ceu_key"  # Contains the private key
 # %%
 # Make sure our key files exist before proceeding
 assert Path.exists(PRIVATE_KEY_FILE)
@@ -18,7 +19,7 @@ with open(PRIVATE_KEY_FILE, "r", encoding="utf-8") as key_file:
     private_key = RSA.import_key(key_file.read())
 
 # %%
-ENCRYPTED_MESSAGE_FILE = PROJECT_FOLDER / "encrypted_message.bin"
+ENCRYPTED_MESSAGE_FILE = PROJECT_FOLDER / "encrypted_message_assignment1.bin"
 # Read the encrypted message from file
 with open(ENCRYPTED_MESSAGE_FILE, "rb") as f:
     encrypted_message_from_file = f.read()
